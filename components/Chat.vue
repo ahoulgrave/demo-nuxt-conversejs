684d9d5c0a816b98f45947683241b911b04d4232
Agustin Houlgrave <a.houlgrave@gmail.com>
2021-05-19 22:31:00 +0200

    initial commit

---
+++ components/Chat.vue
@@ -0,0 +1,11 @@
+<template></template>
+<script>
+export default {
+  created () {
+    window.converse.initialize({
+        bosh_service_url: 'https://conversejs.org/http-bind/',
+        show_controlbox_by_default: true
+    })
+  }
+}
+</script>
